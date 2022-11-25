f11018cd13c96fd300be71478479b41cc8019f80
Lyndsey Boonekamp <lboonekamp@temperzone.com>
2022-11-25 15:57:13 +1300

    updating

--- Server-based App/server.mjs
+++ Server-based App/server.mjs
@@ -1,19 +1,19 @@
-import * as express from "express";
+import express from "express";
 import http from 'http';
-import { readFile } from "fs/promises";
-import { updateIFSConsignment } from "./components/App.Pack";
-import * as config from './webapp-config.json';
-
-const certificate = {
-    key:  await readFile('privatekey.pem'),
-    cert: await readFile('cert.pem')
-};
+import { updateIFSConsignment } from "./components/App.Pack.mjs";
+import * as config from './webapp-config.json' assert { type: 'json' };
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/api/UpdateIFS', updateIFSConsignment);
+app.use((req, res, next) => {
+    res.removeHeader('X-Powered-By');
+    next()
+})
 
-http.createServer(certificate, app)
-    .listen(443, config.hostname, () => console.log('Server listening listening...'))
+app.get('/', (req, res) => res.send({ message: 'ok' }));
+
+app.post('/api/UpdateConsignment', updateIFSConsignment);
+
+http.createServer({}, app).listen(80, config.hostname, () => console.log('Server listening listening...'))
