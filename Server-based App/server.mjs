9309549655c08dae50ae4abe48833b4394977966
Lyndsey Boonekamp <lboonekamp@temperzone.com>
2022-11-28 09:14:09 +1300

    updating

--- Server-based App/server.mjs
+++ Server-based App/server.mjs
@@ -1,10 +1,11 @@
 import express from "express";
 import http from 'http';
 import { updateIFSConsignment } from "./components/App.Pack.mjs";
-import { buildValidateClient, removeHeaders } from "./components/Api.Middleware.mjs";
+import { buildValidateClient, buildValidateFunctionKey, removeHeaders } from "./components/Api.Middleware.mjs";
 import * as config from './webapp-config.json' assert { type: 'json' };
 
 const validateClient = buildValidateClient();
+const validateFunctionKey = buildValidateFunctionKey(config.functionKey);
 
 const app = express();
 
@@ -14,6 +15,6 @@ app.use(removeHeaders);
 
 app.get('/', (req, res) => res.send({ message: 'ok' }));
 
-app.post('/api/UpdateConsignment', validateClient, updateIFSConsignment);
+app.post('/api/UpdateConsignment', validateClient, validateFunctionKey, updateIFSConsignment);
 
 http.createServer({}, app).listen(80, config.hostname, () => console.log('Server listening...'))
